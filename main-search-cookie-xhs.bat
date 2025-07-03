4a65bb77004b26632edcb9f9cbc8cfa1be483609
Jesse <jessejunjing@163.com>
2025-07-03 14:56:36 +0800

    1，更新一波代码；

---
+++ main-search-cookie-xhs.bat
@@ -0,0 +1,11 @@
+@echo off
+REM �������� Conda ������py310Ĭ�ϻ��������֮����ϻ�ȡ��venv��ʱ������ʱ����
+call conda activate py396-venv_20250703_115623-MediaCrawler
+
+REM ���� Python �ű�
+python main.py --platform xhs --lt cookie --type search
+
+REM �ر� Conda ����
+call conda deactivate
+
+PAUSE
