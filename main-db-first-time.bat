1cf1e66b8bc9f00926de6dcff833ea1173d268d8
Jesse <jessejunjing@163.com>
2025-07-04 17:56:42 +0800

    1，创建db-first-time.bat文件；

---
+++ main-db-first-time.bat
@@ -0,0 +1,11 @@
+@echo off
+REM �������� Conda ������py310Ĭ�ϻ��������֮����ϻ�ȡ��venv��ʱ������ʱ����
+call conda activate py396-venv_20250703_115623-MediaCrawler
+
+REM ���� Python �ű�
+python db.py
+
+REM �ر� Conda ����
+call conda deactivate
+
+PAUSE
